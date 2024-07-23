d979afacf66d361e00dc76c123436e80eb3049e2
chaitra2024 <rai.chaitra@gmail.com>
2024-07-23 17:17:41 +0530

    statistics calculate

--- statistics.test.py
+++ statistics.test.py
@@ -1,20 +1,20 @@
 import unittest
-import statistics
-
+import statistics_calculate
+import math
+ 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
-    computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
+    computedStats = statistics_calculate.calculateStats([1.5, 8.9, 3.2, 4.5])
     epsilon = 0.001
     self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
     self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
-
+ 
   def test_avg_is_nan_for_empty_input(self):
-    computedStats = statistics.calculateStats([])
-    # All fields of computedStats (average, max, min) must be
-    # nan (not-a-number), as defined in the math package
-    # Design the assert here.
-    # Use nan and isnan in https://docs.python.org/3/library/math.html
-
+    computedStats = statistics_calculate.calculateStats([])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
+ 
 if __name__ == "__main__":
   unittest.main()
